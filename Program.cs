c0feef8bdcd8059a6a384a96854542024c7615ac
Avivapradasyah <pradasyahaviva@gmail.com>
2022-03-23 06:52:17 -0700

    commit pertama

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiBaru
+{
+    class Program
+    {
+            static void Main(string[] args)
+        {
+                // membuat objek Taxi
+                Taxi taxi = new Taxi();
+
+                // pengesetan nilai properties
+                taxi.DriverName = "abdul";
+                taxi.OnDuty = true;
+                taxi.NumPassenger = 10;
+
+                // pengembalian method
+                taxi.TaxiInfo();
+                taxi.PickUpPassenger();
+                taxi.DropOffPassenger();
+
+                Console.ReadKey();
+           
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiBaru
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        bool Duty = true;
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", Duty ? "yes" : "no");
+            Console.WriteLine(" Number Of Passenger : {0} ", NumPassenger);
+
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang \n", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar pemumpang \n", DriverName);
+        }
+    }
+}
